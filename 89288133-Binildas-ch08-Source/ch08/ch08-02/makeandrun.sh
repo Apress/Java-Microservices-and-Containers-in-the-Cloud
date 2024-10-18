515a978a8bd76c197071b8ebeb486e39ce719eb7
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2024-10-18 14:58:53 -0400

    Add files via upload

---
+++ 89288133-Binildas-ch08-Source/ch08/ch08-02/makeandrun.sh
@@ -0,0 +1,8 @@
+mvn -Dmaven.test.skip=true clean package
+docker build  --build-arg JAR_FILE=02-ProductWeb/target/*.jar -t ecom/product-web .
+docker build  --build-arg JAR_FILE=01-ProductServer/target/*.jar -t ecom/product-server .
+docker network create ecom-network
+docker pull mongo:5.0.25
+docker run -d -it -p 27017:27017 --name mongo  --net=ecom-network  mongo:5.0.25
+docker run -d -p 8081:8081  --net=ecom-network --name product-server ecom/product-server
+docker run -d -p 8080:8080  --net=ecom-network --name product-web ecom/product-web
